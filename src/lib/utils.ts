2c88ab2ec9fa94aa329c253e0c70e88dc702ceaf
khoirulanam-dev <e32240046@student.polije.ac.id>
2025-11-03 13:56:11 +0700

    fix length username

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 // Validasi username: max 30 karakter, tanpa emoji, tanpa karakter berbahaya, hanya huruf, angka, _ . -
 export function isValidUsername(username: string): string | null {
   // Tidak boleh lebih dari 30 karakter
-  if (username.length > 35) {
+  if (username.length > 40) {
     return 'Username must be at most 30 characters.'
   }
   // Tidak boleh kurang dari 3 karakter
