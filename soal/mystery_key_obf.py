a0e747b36944c361f5e7d0c41b9443a4eb6c3cc4
khoirulanam-dev <e32240046@student.polije.ac.id>
2025-11-20 12:13:57 +0700

    fix myster

--- soal/mystery_key_obf.py
+++ soal/mystery_key_obf.py
@@ -1,6 +1,6 @@
 import zlib, base64
 
-print(
+exec(
     zlib.decompress(
         base64.b64decode(
             'eJxz83F0V7BVUArw9/H0cq32zy7JT0otig+K96tMjHcsSLSvVeIqSswsTlVwrUhOLSjJzM/TUPLLL0hV0uQCAEiqE3M='
